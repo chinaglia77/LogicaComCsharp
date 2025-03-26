b1f89a95e99c349ec20da138eea9513a0bf64ec2
Chinaglia <chinagliavictor47@gmail.com>
2025-03-26 11:40:02 -0300

    Encontrar numero sucessor e antecessor
    Programa que exibi o antecessor e o sucessor de um numero.

---
+++ Antecessor_sucessor_numero/Antecessor_sucessor_numero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antecessor_sucessor_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, numeroSucessor, numeroAntecessor;
+
+            Console.Write("Digite o numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            numeroAntecessor = numero - 1;
+            numeroSucessor = numero + 1;
+
+            Console.WriteLine("Antecessor: " + numeroAntecessor);
+            Console.WriteLine("Numero: " + numero);
+            Console.WriteLine("Sucessor: " + numeroSucessor);
+
+        }
+    }
+}
