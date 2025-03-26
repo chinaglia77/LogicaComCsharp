4ce0102ff37ef6c7688d7c494168d82e5239d803
Chinaglia <chinagliavictor47@gmail.com>
2025-03-26 11:19:29 -0300

    Calculo da multa por excesso de peso de peixe
    Este cálculo tem como objetivo determinar o valor da multa que deve ser paga quando o peso do peixe transportado ultrapassa o limite permitido por regulamentações locais ou governamentais.

---
+++ Joao_pescador_peso_peixe/Joao_pescador_peso_peixe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joao_pescador_peso_peixe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, sobra;
+
+            Console.Write("Digite o peso pescado: ");
+            peso = double.Parse(Console.ReadLine());
+
+            sobra = (peso - 50) * 4;
+            Console.Write(sobra);
+        }
+    }
+}
