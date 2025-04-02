c2952c7d113abde8f6e04178061ff7f252c2940c
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2025-04-02 09:50:52 -0300

    Exercicios C#

---
+++ Exercicios03/Quantidade_anos/Quantidade_anos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantidade_anos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int anoNascimento, anoAtual, idadeAnos, idadeMeses, idadeDias;
+
+                Console.Write("Digite o ano de nascimento: ");
+                anoNascimento = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o ano atual: ");
+                anoAtual = int.Parse(Console.ReadLine());
+
+                idadeAnos = anoAtual - anoNascimento;
+                idadeMeses = idadeAnos * 12;
+                idadeDias = idadeAnos * 365;
+
+                // Imprimir idade em anos, meses e dias
+                Console.WriteLine("Você tem " + idadeAnos + " anos, " + idadeMeses + " meses e " + idadeDias + " dias de vida.");
+            }
+        }
+
+    }
+
---
+++ Exercicios03/Trocar_variaveis/Trocar_variaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trocar_variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, temp;
+
+            Console.Write("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+
+            // Trocar os valores de A e B
+            temp = A;
+            A = B;
+            B = temp;
+
+            // Imprimir os valores trocados
+            Console.WriteLine("Após a troca, o valor de A é: " + A + " e o valor de B é: " + B);
+        }
+    }
+}
