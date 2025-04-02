35419cfdf0f6b1e762e19fd83562c66ea08a8244
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2025-04-02 10:30:34 -0300

    Exercicios C#

---
+++ Exercicios03/Fahrenheit_Celsius/Fahrenheit_Celsius/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrenheit_Celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit, celsius;
+
+            Console.Write("Fahrenheit: ");
+            fahrenheit = double.Parse(Console.ReadLine());
+
+            celsius = (5 * (fahrenheit - 32 ) / 9);
+            Console.WriteLine("Celsius: " + celsius.ToString("F2"));
+        }
+    }
+}
