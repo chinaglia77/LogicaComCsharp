3c7a1f1e7be667fc0bd986cee078397167a93227
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2025-04-03 09:53:49 -0300

    Exercicios C#

--- Exercicios03/Calcular_IMC/Calcular_IMC/Program.cs
+++ Exercicios03/Calcular_IMC/Calcular_IMC/Program.cs
@@ -27,8 +27,29 @@ namespace Calcular_IMC
         {
 
 
+            // Exibindo o arte ASCII
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@"
+            ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+            ║                                                                                                                                                                    ║
+            ║                                                                                                                                                                    ║
+            ║                                                                $$$$$$\ $$\      $$\  $$$$$$\                                                                       ║
+            ║                                                                \_$$  _|$$$\    $$$ |$$  __$$\                                                                      ║        
+            ║                                                                  $$ |  $$$$\  $$$$ |$$ /  \__|                                                                     ║
+            ║                                                                  $$ |  $$\$$\$$ $$ |$$ |                                                                           ║
+            ║                                                                  $$ |  $$ \$$$  $$ |$$ |                                                                           ║           
+            ║                                                                  $$ |  $$ |\$  /$$ |$$ |  $$\                                                                      ║
+            ║                                                                $$$$$$\ $$ | \_/ $$ |\$$$$$$  |                                                                     ║
+            ║                                                                \______|\__|     \__| \______/                                                                      ║
+            ║                                                                                                                                                                    ║   
+            ║                                                                                                                                                                    ║
+            ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
 
-            1
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Faça um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma pessoa, \r\nleia o seu peso e sua altura e imprima na tela sua condição \r\nde acordo com a tabela abaixo:\r\nFórmula do IMC = peso / (altura) ²\r\nTabela Condições IMC\r\n\r\nAbaixo de 18,5   | Abaixo do peso\r\nEntre 18,6 e 24,9 | Peso ideal (parabéns)  \r\nEntre 25,0 e 29,9 | Levemente acima do peso\r\nEntre 30,0 e 34,9 | Obesidade grau I \r\nEntre 35,0 e 39,9 | Obesidade grau II (severa)\r\nMaior ou igual a 40 | Obesidade grau III (mórbida)");
+            Console.ResetColor();
+
+            
             double peso, altura, imc;
 
             Console.Write("Digite sua altura: ");
@@ -42,32 +63,32 @@ namespace Calcular_IMC
             if (imc <= 18.5)
             {
                 Console.WriteLine("Abaixo do peso");
-                Console.WriteLine("IMC: " + imc);
+                Console.WriteLine("IMC: " + imc.ToString("F2"));
             }
             else if (imc >= 18.6 && imc <= 24.9)
             {
                 Console.WriteLine("Peso ideal (Parabéns)");
-                Console.WriteLine("IMC: " + imc);
+                Console.WriteLine("IMC: " + imc.ToString("F2"));
             }
             else if (imc >= 25 && imc <= 29.9)
             {
                 Console.WriteLine("Levemente acima do peso");
-                Console.WriteLine("IMC: " + imc);
+                Console.WriteLine("IMC: " + imc.ToString("F2"));
             }
             else if (imc >= 30 && imc <= 34.9 ) 
             {
                 Console.WriteLine("Obesidade I");
-                Console.WriteLine("IMC: " + imc);
+                Console.WriteLine("IMC: " + imc.ToString("F2"));
             }
             else if (imc >= 35 && imc <=39.9)
             {
                 Console.WriteLine("Obesidade II (severa)");
-                Console.WriteLine("IMC: " + imc);
+                Console.WriteLine("IMC: " + imc.ToString("F2"));
             }
             else
             {
                 Console.WriteLine("Obesidade III (mórbida)");
-                Console.WriteLine("IMC: " + imc);
+                Console.WriteLine("IMC: " + imc.ToString("F2"));
             }
         }
     }
